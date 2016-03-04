3b8c209211670586c6a8f517dedf1dc447b96f19
Kentaro Takiguchi <kentaro-takiguchi@cookpad.com>
2016-03-04 15:32:24 +0900

    Add tests

--- example/src/androidTest/java/com/example/android/kvs/ExamplePrefsTest.java
+++ example/src/androidTest/java/com/example/android/kvs/ExamplePrefsTest.java
@@ -15,17 +15,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(AndroidJUnit4.class)
 public class ExamplePrefsTest {
 
+    private Context context;
     private ExamplePrefs prefs;
 
     @Before
     public void setup() {
-        Context context = InstrumentationRegistry.getTargetContext();
-        prefs = ExamplePrefs.get(context);
+        this.context = InstrumentationRegistry.getTargetContext();
+        this.prefs = ExamplePrefs.get(context);
+        prefs.clear();
+    }
+
+    @Test
+    public void getSingletonInstance() {
+        {
+            ExamplePrefs prefs1 = new ExamplePrefs(context);
+            ExamplePrefs prefs2 = new ExamplePrefs(context);
+            assertThat(prefs1).isNotEqualTo(prefs2);
+        }
+        {
+            ExamplePrefs prefs1 = ExamplePrefs.get(context);
+            ExamplePrefs prefs2 = ExamplePrefs.get(context);
+            assertThat(prefs1).isEqualTo(prefs2);
+        }
     }
 
     @Test
     public void checkTypeLongWorks() {
-        prefs.clear();
         assertThat(prefs.hasUserId()).isFalse();
 
         prefs.putUserId(99999999999L);
@@ -38,7 +53,6 @@ public class ExamplePrefsTest {
 
     @Test
     public void checkTypeStringWorks() {
-        prefs.clear();
         assertThat(prefs.hasUserName()).isFalse();
         assertThat(prefs.getUserName("guest")).isEqualTo("guest");
 
@@ -52,7 +66,6 @@ public class ExamplePrefsTest {
 
     @Test
     public void checkTypeIntWorks() {
-        prefs.clear();
         assertThat(prefs.hasUserAge()).isFalse();
 
         prefs.putUserAge(26);
@@ -65,7 +78,6 @@ public class ExamplePrefsTest {
 
     @Test
     public void checkTypeBooleanWorks() {
-        prefs.clear();
         assertThat(prefs.hasGuestFlag()).isFalse();
 
         prefs.putGuestFlag(true);
@@ -79,7 +91,6 @@ public class ExamplePrefsTest {
 
     @Test
     public void checkTypeStringSetWorks() {
-        prefs.clear();
         assertThat(prefs.hasSearchHistory()).isFalse();
 
         prefs.putSearchHistory(new HashSet<String>() {{
@@ -89,6 +100,9 @@ public class ExamplePrefsTest {
         }});
         assertThat(prefs.hasSearchHistory()).isTrue();
         assertThat(prefs.getSearchHistory().size()).isEqualTo(3);
+        assertThat(prefs.getSearchHistory().contains("JAVA")).isTrue();
+        assertThat(prefs.getSearchHistory().contains("+")).isTrue();
+        assertThat(prefs.getSearchHistory().contains("YOU")).isTrue();
 
         prefs.removeSearchHistory();
         assertThat(prefs.hasSearchHistory()).isFalse();

d3055914ad0d461f898d34971558d37607643231
Kentaro Takiguchi <kentaro-takiguchi@cookpad.com>
2016-05-01 16:54:23 +0900

    Crete TypeUtils

--- compiler/src/main/java/com/rejasupotaro/android/kvs/internal/Field.java
+++ compiler/src/main/java/com/rejasupotaro/android/kvs/internal/Field.java
@@ -5,17 +5,11 @@ import com.rejasupotaro.android.kvs.serializers.DefaultPrefsSerializer;
 import com.rejasupotaro.android.kvs.serializers.PrefsSerializer;
 import com.squareup.javapoet.TypeName;
 
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.util.List;
-
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.MirroredTypeException;
-import javax.lang.model.type.TypeKind;
-import javax.lang.model.type.TypeMirror;
 
 public class Field {
     private String prefKeyName;
@@ -74,63 +68,12 @@ public class Field {
     }
 
     private TypeName detectSerializeTypeNameByClass(Class clazz) {
-        TypeName serializeType = TypeName.get(getSerializerGenericsTypesByClass(clazz)[0]);
-        try {
-            serializeType = serializeType.unbox();
-        } catch (UnsupportedOperationException ignore) {
-        }
-        return serializeType;
+        TypeName serializeType = TypeName.get(TypeUtils.getGenericsTypesByClass(clazz, PrefsSerializer.class)[0]);
+        return TypeUtils.unbox(serializeType);
     }
 
     private TypeName detectSerializeTypeByTypeElement(TypeElement element) {
-        TypeName serializeType = TypeName.get(getSerializerGenericsTypesByTypeElement(element).get(0));
-        try {
-            serializeType = serializeType.unbox();
-        } catch (UnsupportedOperationException ignore) {
-        }
-        return serializeType;
-    }
-
-    private Type[] getSerializerGenericsTypesByClass(Class clazz) {
-        Type type = PrefsSerializer.class;
-        Class targetClass = clazz;
-        while (targetClass != null) {
-            for (Type in : targetClass.getGenericInterfaces()) {
-                if (in instanceof ParameterizedType &&
-                        type.equals(((ParameterizedType) in).getRawType())) {
-                    return ((ParameterizedType) in).getActualTypeArguments();
-                }
-            }
-            Type superClazz = targetClass.getGenericSuperclass();
-            if (superClazz instanceof ParameterizedType &&
-                    type.equals(((ParameterizedType) superClazz).getRawType())) {
-                return ((ParameterizedType) superClazz).getActualTypeArguments();
-            }
-            targetClass = targetClass.getSuperclass();
-        }
-        throw new UnsupportedOperationException("Not found serializer type: " + clazz.getName());
-    }
-
-    private List<? extends TypeMirror> getSerializerGenericsTypesByTypeElement(TypeElement element) {
-        TypeElement targetType = element;
-        while (targetType != null) {
-            if (!targetType.getInterfaces().isEmpty()) {
-                for (TypeMirror in : targetType.getInterfaces()) {
-                    DeclaredType inType = (DeclaredType) in;
-                    if (inType.asElement().getSimpleName().toString()
-                            .equals(PrefsSerializer.class.getSimpleName())) {
-                        return inType.getTypeArguments();
-                    }
-                }
-            }
-
-            TypeMirror superClassType = targetType.getSuperclass();
-            if (superClassType.getKind().equals(TypeKind.DECLARED)) {
-                targetType = (TypeElement) ((DeclaredType) superClassType).asElement();
-            } else {
-                break;
-            }
-        }
-        throw new UnsupportedOperationException("Not found serializer type: " + element.toString());
+        TypeName serializeType = TypeName.get(TypeUtils.getGenericsTypesByTypeElement(element, PrefsSerializer.class).get(0));
+        return TypeUtils.unbox(serializeType);
     }
 }
